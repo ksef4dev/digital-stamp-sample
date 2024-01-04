452e54c0fc22dd7dd918641e397891196483c2f9
Adrian Lapierre <al@alapierre.io>
2024-01-04 08:10:09 +0100

    Remove unneeded files and clean up imports

--- src/main/java/io/alapierre/sample/ksef/Main.java
+++ src/main/java/io/alapierre/sample/ksef/Main.java
@@ -1,16 +1,11 @@
 package io.alapierre.sample.ksef;
 
-import eu.europa.esig.dss.token.PasswordInputCallback;
-import eu.europa.esig.dss.token.PrefilledPasswordCallback;
-import io.alapierre.crypto.dss.signer.CardSigner;
 import io.alapierre.crypto.dss.signer.P12Signer;
 import io.alapierre.ksef.api.dss.facade.KsefDssFacade;
-import io.alapierre.ksef.client.AbstractApiClient;
 import io.alapierre.ksef.client.ApiClient;
 import io.alapierre.ksef.client.ApiException;
 import io.alapierre.ksef.client.JsonSerializer;
 import io.alapierre.ksef.client.api.InterfejsyInteraktywneSesjaApi;
-import io.alapierre.ksef.client.model.rest.auth.AuthorisationChallengeRequest;
 import io.alapierre.ksef.client.model.rest.auth.InitSignedResponse;
 import io.alapierre.ksef.client.okhttp.OkHttpApiClient;
 import io.alapierre.ksef.client.serializer.gson.GsonJsonSerializer;
@@ -20,7 +15,7 @@ import java.io.IOException;
 import java.security.KeyStore;
 
 import static io.alapierre.ksef.client.AbstractApiClient.Environment.TEST;
-import static io.alapierre.ksef.client.model.rest.auth.AuthorisationChallengeRequest.IdentifierType.*;
+import static io.alapierre.ksef.client.model.rest.auth.AuthorisationChallengeRequest.IdentifierType.onip;
 
 /**
  * @author Adrian Lapierre {@literal al@alapierre.io}
